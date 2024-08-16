07dbc809d216b472fa0992628a479d3357f37a7b
chanv64 <chanv64@yahoo.com>
2024-08-16 18:38:21 +0800

    update init.lua

--- init.lua
+++ init.lua
@@ -204,6 +204,21 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+local lastplace = vim.api.nvim_create_augroup('LastPlace', {})
+vim.api.nvim_clear_autocmds { group = lastplace }
+vim.api.nvim_create_autocmd('BufReadPost', {
+  group = lastplace,
+  pattern = { '*' },
+  desc = 'remember last cursor place',
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
